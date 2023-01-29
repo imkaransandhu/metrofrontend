54284c1b31c7066a22cbc7825f567973bc166d07
Karan Singh <70357137+imkaransandhu@users.noreply.github.com>
2023-01-30 00:13:08 +1300

    Fix: router path bu reoving /

--- src/App.js
+++ src/App.js
@@ -42,68 +42,65 @@ const App = () => {
         <UserNavbar />
         {/* our routes */}
         <Routes>
-          <Route path="/metrofrontend" element={<Home />} />
+          <Route path="metrofrontend" element={<Home />} />
           <Route
-            path="/metrofrontend/rental-process"
+            path="metrofrontend/rental-process"
             element={<RentalProcess />}
           />
           <Route
-            path="/metrofrontend/tenant-services"
+            path="metrofrontend/tenant-services"
             element={<TenantServices />}
           />
           <Route
-            path="/metrofrontend/buy-a-home-while-renting"
+            path="metrofrontend/buy-a-home-while-renting"
             element={<BuyHomeWhileRenting />}
           />
           <Route
-            path="/metrofrontend/meet-our-professionals"
+            path="metrofrontend/meet-our-professionals"
             element={<MeetOurProfessionals />}
           />
           <Route
-            path="/metrofrontend/property-search"
+            path="metrofrontend/property-search"
             element={<PropertySearch />}
           />
 
-          <Route exact path="login" element={<Login />} />
-          <Route path="register" element={<Signup />} />
+          <Route exact path="metrofrontend/login" element={<Login />} />
+          <Route path="metrofrontend/register" element={<Signup />} />
           {/* My Dairy Site */}
           <Route
-            path="/metrofrontend/welcome-board-1"
+            path="metrofrontend/welcome-board-1"
             element={<WelcomeBoard1 />}
           />
           <Route
-            path="/metrofrontend/welcome-board-2"
+            path="metrofrontend/welcome-board-2"
             element={<WelcomeBoard2 />}
           />
+          <Route path="metrofrontend/my-dairy/new-user" element={<NewUser />} />
           <Route
-            path="/metrofrontend/my-dairy/new-user"
-            element={<NewUser />}
-          />
-          <Route
-            path="/metrofrontend/my-dairy/existing-user"
+            path="metrofrontend/my-dairy/existing-user"
             element={<ExistingUser />}
           />
-          <Route path="/metrofrontend/my-dairy" element={<NewUser />} />
+          <Route path="metrofrontend/my-dairy" element={<NewUser />} />
           <Route
-            path="/metrofrontend/my-dairy/my-account"
+            path="metrofrontend/my-dairy/my-account"
             element={<Md1Account />}
           />
           <Route
-            path="/metrofrontend/my-dairy/my-favorite"
+            path="metrofrontend/my-dairy/my-favorite"
             element={<Md2Favorite />}
           />
           <Route
-            path="/metrofrontend/my-dairy/my-applications"
+            path="metrofrontend/my-dairy/my-applications"
             element={<Md2Applications />}
           />
 
           {/* Tenancy Application Site */}
           <Route
-            path="/metrofrontend/tenancy-application"
+            path="metrofrontend/tenancy-application"
             element={<TAYourDetail />}
           />
           <Route
-            path="/metrofrontend/tenancy-application-complete"
+            path="metrofrontend/tenancy-application-complete"
             element={<TAComplete />}
           />
         </Routes>
